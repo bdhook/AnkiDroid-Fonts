541cd611248d66457dbb4e174c3a58f77853ab54
Bradley Hook <bdhook@gmail.com>
2012-09-30 18:20:26 -0500

    Initial Code Commit

---
+++ AnkiDroidFonts/gen/org/coder7/ankidroidfonts/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.coder7.ankidroidfonts;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ AnkiDroidFonts/src/org/coder7/ankidroidfonts/MainActivity.java
@@ -0,0 +1,97 @@
+/*
+ *  AnkiDroid Fonts is a simple GPL font installer for AnkiDroid
+ *  Copyright (C) 2012 Bradley Hook
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+ * */
+package org.coder7.ankidroidfonts;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    public void installFonts(View view) {
+
+    	AssetManager assetManager = getAssets();
+    	String filename = "FreeSerif.ttf";
+    	InputStream in = null;
+    	OutputStream out = null;
+    	try {
+    		in = assetManager.open(filename);
+    		String target_path=Environment.getExternalStorageDirectory().getAbsolutePath() + "/AnkiDroid/fonts/" + filename;
+
+    		Context context = getApplicationContext();
+    		int duration = Toast.LENGTH_SHORT;
+
+    		CharSequence text="Attempting to copy " + filename + " to " + target_path;
+    		Toast toast = Toast.makeText(context, text, duration);
+    		toast.show();
+
+    		out = new FileOutputStream(target_path);
+    		copyFile(in, out);
+    		in.close();
+    		in = null;
+    		out.flush();
+    		out.close();
+    		out = null;
+    		text="File seems to have copied successfully.";
+    		toast = Toast.makeText(context, text, duration);
+    		toast.show();
+
+    	} catch(Exception e) {
+    		Context context = getApplicationContext();
+    		int duration = Toast.LENGTH_SHORT;
+
+    		CharSequence text="Copy failed. "+e.getMessage();
+    		Toast toast = Toast.makeText(context, text, duration);
+    		toast.show();
+
+    		Log.e("tag", e.getMessage());
+    	}       
+
+    }
+    
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+          out.write(buffer, 0, read);
+        }
+    }
+}
